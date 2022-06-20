52bada8f1d74564e634dd1247cd8d81e16b5f476
Jeeyong Um <conr2d@gmail.com>
2022-06-20 22:07:42 +0800

    feat(fmt): add std::map<K, V> formatter

--- src/eo/fmt.h
+++ src/eo/fmt.h
@@ -1,21 +1,55 @@
 #pragma once
 #include <eo/core.h>
 
-#include <fmt/core.h>
+#include <fmt/format.h>
 
 namespace fmt {
 
-template<typename T, typename... Ts>
-void println(T&& format_str, Ts&&... args) {
-  fmt::print(std::forward<T>(format_str), std::forward<Ts>(args)...);
+template<typename... T>
+void println(format_string<T...> fmt, T&&... args) {
+  fmt::print(fmt, std::forward<T>(args)...);
   fmt::print("\n");
 }
 
-template<typename T, typename... Ts>
-void fprintln(std::FILE* f, T&& format_str, Ts&&... args) {
-  fmt::print(f, std::forward<T>(format_str), std::forward<Ts>(args)...);
+template<typename T>
+void println(T&& arg) {
+  fmt::print("{}", arg);
+  fmt::print("\n");
+}
+
+template<typename... T>
+void fprintln(std::FILE* f, format_string<T...> fmt, T&&... args) {
+  fmt::print(f, fmt, std::forward<T>(args)...);
   fmt::print(f, "\n");
 }
+
+template<typename T>
+void fprintln(std::FILE* f, T&& arg) {
+  fmt::print(f, "{}", arg);
+  fmt::print(f, "\n");
+}
+
+template<typename K, typename V>
+struct formatter<std::pair<K, V>> {
+  constexpr auto parse(format_parse_context& ctx) -> decltype(ctx.begin()) {
+    return ctx.end();
+  }
+  template<typename FormatContext>
+  auto format(const std::pair<K, V>& p, FormatContext& ctx) {
+    return format_to(ctx.out(), "{}:{}", p.first, p.second);
+  }
+};
+
+template<typename K, typename V>
+struct formatter<std::map<K, V>> {
+  constexpr auto parse(format_parse_context& ctx) -> decltype(ctx.begin()) {
+    return ctx.end();
+  }
+  template<typename FormatContext>
+  auto format(const std::map<K, V>& p, FormatContext& ctx) {
+    return format_to(ctx.out(), "map[{}]", join(p, " "));
+  }
+};
 
 } // namespace fmt
 
