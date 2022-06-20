cdf4a92212f5ffdf52a5d69443d87bf0108340d6
Jeeyong Um <conr2d@gmail.com>
2022-06-20 12:38:21 +0800

    feat(fmt): add fprintln

--- src/eo/fmt.h
+++ src/eo/fmt.h
@@ -11,6 +11,12 @@ void println(T&& format_str, Ts&&... args) {
   fmt::print("\n");
 }
 
+template<typename T, typename... Ts>
+void fprintln(std::FILE* f, T&& format_str, Ts&&... args) {
+  fmt::print(f, std::forward<T>(format_str), std::forward<Ts>(args)...);
+  fmt::print(f, "\n");
+}
+
 } // namespace fmt
 
 namespace eo {
