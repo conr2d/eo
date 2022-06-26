a5c650c3c98c51e72db30325edc3c5c9603e4a00
Jeeyong Um <conr2d@gmail.com>
2022-06-26 15:34:47 +0800

    example: add default-selection

---
+++ examples/default-selection.cpp
@@ -0,0 +1,57 @@
+// https://go.dev/tour/concurrency/6
+//
+// package main
+//
+// import (
+//     "fmt"
+//     "time"
+// )
+//
+// func main() {
+//     tick := time.Tick(100 * time.Millisecond)
+//     boom := time.After(500 * time.Millisecond)
+//     for {
+//         select {
+//         case <-tick:
+//             fmt.Println("tick.")
+//         case <-boom:
+//             fmt.Println("BOOM!")
+//             return
+//         default:
+//             fmt.Println("    .")
+//             time.Sleep(50 * time.Millisecond)
+//         }
+//     }
+// }
+
+#include <eo/fmt.h>
+#include <eo/time.h>
+
+using namespace eo;
+
+func<> eo_main() {
+  auto tick = time::Ticker(std::chrono::milliseconds(100));
+  auto boom = time::Timer(std::chrono::milliseconds(500));
+
+  for (;;) {
+    // auto select = Select{ *tick.ch, *boom.ch, CaseDefault() };
+    // auto index = select.index();
+    auto select = Select{ *tick.ch, *boom.ch };
+    auto index = co_await select.index();
+
+    switch (index) {
+    case 0:
+      co_await select.process<0>();
+      fmt::println("tick.");
+      break;
+    case 1:
+      co_await select.process<1>();
+      fmt::println("BOOM!");
+      co_return;
+    default:
+      fmt::println("    .");
+      co_await time::sleep(std::chrono::milliseconds(50));
+      break;
+    }
+  }
+}
