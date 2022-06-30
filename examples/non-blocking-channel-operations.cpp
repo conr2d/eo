66f958f88487a3d13818e1bc525e3e0d1759b40c
Jeeyong Um <conr2d@gmail.com>
2022-07-01 02:26:25 +0800

    example: update non-blocking-channel-operations

--- examples/non-blocking-channel-operations.cpp
+++ examples/non-blocking-channel-operations.cpp
@@ -45,8 +45,8 @@ func<> eo_main() {
     auto select = Select{*messages, CaseDefault{}};
     switch (co_await select.index()) {
     case 0: {
-      auto v = co_await select.process<0>();
-      fmt::println("received message {}", v);
+      auto msg = co_await select.process<0>();
+      fmt::println("received message {}", msg);
       break;
     }
     default:
@@ -73,13 +73,17 @@ func<> eo_main() {
   {
     auto select = Select{*messages, *signals, CaseDefault{}};
     switch (co_await select.index()) {
-    case 0:
-      fmt::println("received message {}", co_await select.process<0>());
+    case 0: {
+      auto msg = co_await select.process<0>();
+      fmt::println("received message {}", msg);
       break;
-    case 1:
-      fmt::println("received signal {}", co_await select.process<1>());
+    }
+    case 1: {
+      auto signal = co_await select.process<1>();
+      fmt::println("received signal {}", signal);
       break;
-    case 2:
+    }
+    default:
       fmt::println("no activity");
       break;
     }
