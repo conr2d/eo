928c4521de814b6aadc9be30f627028ac52eb43b
Jeeyong Um <conr2d@gmail.com>
2022-06-30 19:12:11 +0800

    fix(math): make rng thread-safe

--- src/eo/math/rand.cpp
+++ src/eo/math/rand.cpp
@@ -3,12 +3,10 @@
 //
 #include <eo/math/rand.h>
 #include <random>
-#include <shared_mutex>
 
 namespace eo::math::rand {
 
-std::shared_mutex mtx;
-std::mt19937 rng = []() {
+thread_local std::mt19937 rng = []() {
   std::mt19937 rng(std::random_device{}());
   rng.discard(700000);
   return rng;
@@ -18,7 +16,6 @@ auto int_n(int n) -> int {
   if (n <= 0) {
     throw std::runtime_error("invalid argument to int_n");
   }
-  std::shared_lock g{mtx};
   return std::uniform_int_distribution(0, n - 1)(rng);
 }
 
@@ -26,7 +23,6 @@ auto int63_n(int64_t n) -> int64_t {
   if (n <= 0) {
     throw std::runtime_error("invalid argument to int63_n");
   }
-  std::shared_lock g{mtx};
   return std::uniform_int_distribution<int64_t>(0, n - 1)(rng);
 }
 
