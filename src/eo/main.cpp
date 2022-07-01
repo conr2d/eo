e58bc1cff2e0b88852aaf48fea383a53bc27fe8b
Jeeyong Um <conr2d@gmail.com>
2022-07-01 21:27:13 +0800

    feat(main): propagate an exception

--- src/eo/main.cpp
+++ src/eo/main.cpp
@@ -11,11 +11,17 @@ extern func<> eo_main();
 
 int main(int argc, char** argv) {
   try {
-    boost::asio::co_spawn(runtime::executor, eo_main(), boost::asio::detached);
+    std::exception_ptr _eptr{};
+    boost::asio::co_spawn(runtime::executor, eo_main(), [&](std::exception_ptr eptr) {
+      _eptr = eptr;
+    });
     runtime::executor.join();
-  } catch (boost::exception& e) {
+    if (_eptr) {
+      std::rethrow_exception(_eptr);
+    }
+  } catch (const boost::exception& e) {
     std::cerr << boost::diagnostic_information(e) << std::endl;
-  } catch (std::exception& e) {
+  } catch (const std::exception& e) {
     std::cerr << e.what() << std::endl;
   } catch (...) {
   }
