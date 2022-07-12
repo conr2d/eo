1936c0eea3bbf5471fb4655a7b4d79af5248a8e3
Jeeyong Um <conr2d@gmail.com>
2022-07-12 12:35:59 +0800

    feat(os): add expand_env()

---
+++ src/eo/os.cpp
@@ -0,0 +1,19 @@
+#include <eo/os.h>
+#include <wordexp.h>
+
+namespace eo::os {
+
+auto expand_env(const std::string& s) -> std::string {
+  wordexp_t p;
+  eo_defer([&]() { wordfree(&p); });
+  wordexp(s.data(), &p, 0);
+
+  std::string v{};
+  for (auto i = 0; i < p.we_wordc; ++i) {
+    if (i) v += " ";
+    v += p.we_wordv[i];
+  }
+  return v;
+}
+
+} // namespace eo::os
---
+++ src/eo/os.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2022 Jeeyong "conr2d" Um
+// SPDX-License-Identifier: MIT
+//
+#pragma once
+#include <eo/core.h>
+
+namespace eo::os {
+
+auto expand_env(const std::string& s) -> std::string;
+
+} // namespace eo::os
