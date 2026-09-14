519d92634ae7b031e1beba4ea87035aa9c447f1e
Undeleted user <98804528+4lmighty@users.noreply.github.com>
2026-09-14 23:55:10 +0900

    style: format select send tests

--- tests/select.cpp
+++ tests/select.cpp
@@ -81,8 +81,7 @@ auto select_closed_send(eo::chan<int> ch) -> eo::func<> {
   try {
     co_await select.process<0>();
   } catch (const std::runtime_error& error) {
-    check(
-      std::string{error.what()} == "panic: send on closed channel", "closed selected send returned the wrong error");
+    check(std::string{error.what()} == "panic: send on closed channel", "closed send returned wrong error");
     co_return;
   }
 
