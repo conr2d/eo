b9dbe06f55317f3669752493cce358d75bb84ed0
Undeleted user <98804528+4lmighty@users.noreply.github.com>
2026-09-14 19:43:12 +0900

    fix: make defer independent of nonstandard scope header

--- src/eo/defer.h
+++ src/eo/defer.h
@@ -3,8 +3,25 @@
 //
 #pragma once
 
-#include <scope>
+#include <utility>
+
+namespace eo::detail {
+template <typename F> class scope_exit {
+public:
+  explicit scope_exit(F function) : function_(std::move(function)) {}
+
+  scope_exit(const scope_exit &) = delete;
+  scope_exit &operator=(const scope_exit &) = delete;
+
+  ~scope_exit() noexcept { function_(); }
+
+private:
+  F function_;
+};
+
+template <typename F> scope_exit(F) -> scope_exit<F>;
+} // namespace eo::detail
 
 #define EO_CONCAT_IMPL(a, b) a##b
 #define EO_CONCAT(a, b) EO_CONCAT_IMPL(a, b)
-#define eo_defer(...) auto EO_CONCAT(_eo_defer_, __COUNTER__) = std::scope_exit(__VA_ARGS__)
+#define eo_defer(...) auto EO_CONCAT(_eo_defer_, __COUNTER__) = eo::detail::scope_exit(__VA_ARGS__)
