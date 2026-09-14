7758a2fd4c209b8a74dae9e20ab629ae6981241e
Undeleted user <98804528+4lmighty@users.noreply.github.com>
2026-09-14 20:22:02 +0900

    test: avoid coroutine lambda lifetime hazards

--- tests/channel.cpp
+++ tests/channel.cpp
@@ -4,7 +4,6 @@
 #include <eo/chan.h>
 
 #include <boost/asio/co_spawn.hpp>
-#include <boost/asio/detached.hpp>
 #include <boost/asio/io_context.hpp>
 #include <boost/asio/use_future.hpp>
 
@@ -15,17 +14,34 @@
 
 namespace asio = boost::asio;
 
+auto buffered_roundtrip(eo::chan<int> ch) -> eo::func<int> {
+  assert(co_await (ch << 42));
+  co_return co_await *ch;
+}
+
+auto send_string(eo::chan<std::string> ch) -> eo::func<> {
+  assert(co_await (ch << "ping"));
+  co_return;
+}
+
+auto receive_string(eo::chan<std::string> ch) -> eo::func<std::string> {
+  co_return co_await *ch;
+}
+
+auto receive_int(eo::chan<int> ch) -> eo::func<int> {
+  co_return co_await *ch;
+}
+
+auto send_int(eo::chan<int> ch, int value) -> eo::func<> {
+  co_await (ch << value);
+  co_return;
+}
+
 void test_buffered_send_receive() {
   asio::io_context io;
   eo::chan<int> ch{io.get_executor(), 1};
 
-  auto result = asio::co_spawn(
-    io,
-    [&]() -> eo::func<int> {
-      assert(co_await (ch << 42));
-      co_return co_await *ch;
-    }(),
-    asio::use_future);
+  auto result = asio::co_spawn(io, buffered_roundtrip(ch), asio::use_future);
 
   io.run();
   assert(result.get() == 42);
@@ -35,18 +51,12 @@ void test_unbuffered_send_receive() {
   asio::io_context io;
   eo::chan<std::string> ch{io.get_executor()};
 
-  asio::co_spawn(
-    io,
-    [&]() -> eo::func<> {
-      assert(co_await (ch << "ping"));
-      co_return;
-    }(),
-    asio::detached);
-
-  auto result = asio::co_spawn(io, [&]() -> eo::func<std::string> { co_return co_await *ch; }(), asio::use_future);
+  auto sender = asio::co_spawn(io, send_string(ch), asio::use_future);
+  auto receiver = asio::co_spawn(io, receive_string(ch), asio::use_future);
 
   io.run();
-  assert(result.get() == "ping");
+  sender.get();
+  assert(receiver.get() == "ping");
 }
 
 void test_close_then_receive_zero_value() {
@@ -54,7 +64,7 @@ void test_close_then_receive_zero_value() {
   eo::chan<int> ch{io.get_executor()};
   ch.close();
 
-  auto result = asio::co_spawn(io, [&]() -> eo::func<int> { co_return co_await *ch; }(), asio::use_future);
+  auto result = asio::co_spawn(io, receive_int(ch), asio::use_future);
 
   io.run();
   assert(result.get() == 0);
@@ -65,13 +75,7 @@ void test_send_on_closed_channel_panics() {
   eo::chan<int> ch{io.get_executor()};
   ch.close();
 
-  auto result = asio::co_spawn(
-    io,
-    [&]() -> eo::func<> {
-      co_await (ch << 1);
-      co_return;
-    }(),
-    asio::use_future);
+  auto result = asio::co_spawn(io, send_int(ch, 1), asio::use_future);
 
   io.run();
 
