ea0b6f40b1126a12f530ca7db921acbcc28f4697
Jeeyong Um <conr2d@proton.me>
2026-09-14 15:05:07 +0900

    refactor: use C++23 scope_exit for defer

--- src/eo/defer.h
+++ src/eo/defer.h
@@ -2,7 +2,9 @@
 // SPDX-License-Identifier: MIT
 //
 #pragma once
-#include <boost/preprocessor/cat.hpp>
-#include <nonstd/scope.hpp>
 
-#define eo_defer(...) auto BOOST_PP_CAT(BOOST_PP_CAT(_tmp, __COUNTER__), _) = nonstd::make_scope_exit(__VA_ARGS__)
+#include <scope>
+
+#define EO_CONCAT_IMPL(a, b) a##b
+#define EO_CONCAT(a, b) EO_CONCAT_IMPL(a, b)
+#define eo_defer(...) auto EO_CONCAT(_eo_defer_, __COUNTER__) = std::scope_exit(__VA_ARGS__)
