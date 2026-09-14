f24dd7e5e8d7c94dfcf36a4ef6f2a74c6a07c12b
Undeleted user <98804528+4lmighty@users.noreply.github.com>
2026-09-14 19:48:28 +0900

    fix: make custom fmt formatters const

--- src/eo/fmt.h
+++ src/eo/fmt.h
@@ -39,7 +39,7 @@ struct formatter<std::pair<K, V>> {
     return ctx.end();
   }
   template<typename FormatContext>
-  auto format(const std::pair<K, V>& p, FormatContext& ctx) {
+  auto format(const std::pair<K, V>& p, FormatContext& ctx) const {
     return fmt::format_to(ctx.out(), "{}:{}", p.first, p.second);
   }
 };
@@ -50,7 +50,7 @@ struct formatter<std::map<K, V>> {
     return ctx.end();
   }
   template<typename FormatContext>
-  auto format(const std::map<K, V>& p, FormatContext& ctx) {
+  auto format(const std::map<K, V>& p, FormatContext& ctx) const {
     return fmt::format_to(ctx.out(), "map[{}]", fmt::join(p, " "));
   }
 };
@@ -61,7 +61,7 @@ struct formatter<std::chrono::time_point<Clock>> {
     return ctx.end();
   }
   template<typename FormatContext>
-  auto format(const std::chrono::time_point<Clock>& p, FormatContext& ctx) {
+  auto format(const std::chrono::time_point<Clock>& p, FormatContext& ctx) const {
     auto buffer = std::vector<char>(256);
     auto t = Clock::to_time_t(p);
     auto size = std::strftime(buffer.data(), buffer.size(), "%F %T %z %Z", std::localtime(&t));
