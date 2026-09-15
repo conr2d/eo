4f1c4e7a45c291af6c6ec110d03f8d434bbe3a9e
Undeleted user <98804528+4lmighty@users.noreply.github.com>
2026-09-16 01:39:25 +0900

    test: cover awaited nil channel suspension

--- tests/channel_status.cpp
+++ tests/channel_status.cpp
@@ -4,12 +4,18 @@
 #include <eo/chan.h>
 #include <eo/select.h>
 
+#include <boost/asio/co_spawn.hpp>
 #include <boost/asio/io_context.hpp>
+#include <boost/asio/post.hpp>
+#include <boost/asio/use_future.hpp>
 
+#include <chrono>
+#include <future>
 #include <stdexcept>
 #include <string>
 
 namespace asio = boost::asio;
+using namespace std::chrono_literals;
 
 void check(bool condition, const char* message) {
   if (!condition) {
@@ -37,6 +43,32 @@ void test_close_nil_channel_panics() {
   }
 
   throw std::runtime_error("close(nil) did not panic");
+}
+
+void test_awaited_nil_operations_stay_suspended() {
+  asio::io_context io;
+  eo::chan<int> nil;
+  bool progressed = false;
+
+  auto sender = asio::co_spawn(
+    io,
+    [&]() -> eo::func<> {
+      co_await (nil << 1);
+    },
+    asio::use_future);
+  auto receiver = asio::co_spawn(
+    io,
+    [&]() -> eo::func<> {
+      (void)co_await *nil;
+    },
+    asio::use_future);
+
+  asio::post(io, [&] { progressed = true; });
+  io.poll();
+
+  check(progressed, "nil channel operations blocked unrelated scheduled work");
+  check(sender.wait_for(0s) == std::future_status::timeout, "nil send should remain suspended");
+  check(receiver.wait_for(0s) == std::future_status::timeout, "nil receive should remain suspended");
 }
 
 void test_ready_channel_beats_nil_case() {
@@ -79,6 +111,7 @@ void test_recv2_reports_closed_receive() {
 int main() {
   test_default_constructed_channel_is_nil();
   test_close_nil_channel_panics();
+  test_awaited_nil_operations_stay_suspended();
   test_ready_channel_beats_nil_case();
   test_recv2_reports_successful_receive();
   test_recv2_reports_closed_receive();
