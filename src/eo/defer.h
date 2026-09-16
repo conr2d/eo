d5e33dbb5ece5b4a2e6cb278a37c082dae28fb26
Undeleted user <98804528+4lmighty@users.noreply.github.com>
2026-09-16 11:00:00 +0900

    fix: drain defers before local destruction

--- src/eo/defer.h
+++ src/eo/defer.h
@@ -5,7 +5,6 @@
 
 #include <functional>
 #include <memory>
-#include <tuple>
 #include <type_traits>
 #include <utility>
 #include <vector>
@@ -35,25 +34,18 @@ public:
   auto operator=(const defer_stack&) -> defer_stack& = delete;
   auto operator=(defer_stack&&) -> defer_stack& = delete;
 
-  ~defer_stack() noexcept(false) {
+  template<typename F>
+  void push(F&& function) {
+    using function_type = std::decay_t<F>;
+    calls.emplace_back(std::make_unique<call<function_type>>(function_type(std::forward<F>(function))));
+  }
+
+  void run() {
     while (!calls.empty()) {
       auto deferred = std::move(calls.back());
       calls.pop_back();
       deferred->invoke();
     }
-  }
-
-  template<typename F, typename... Args>
-  void push(F&& function, Args&&... args) {
-    using function_type = std::decay_t<F>;
-    using args_type = std::tuple<std::decay_t<Args>...>;
-
-    auto stored_function = function_type(std::forward<F>(function));
-    auto stored_args = args_type(std::forward<Args>(args)...);
-    auto deferred = [function = std::move(stored_function), args = std::move(stored_args)]() mutable {
-      std::apply(std::move(function), std::move(args));
-    };
-    calls.emplace_back(std::make_unique<call<decltype(deferred)>>(std::move(deferred)));
   }
 
 private:
@@ -63,3 +55,4 @@ private:
 
 #define eo_defer_scope ::eo::detail::defer_stack _eo_defer_stack
 #define eo_defer(...) _eo_defer_stack.push(__VA_ARGS__)
+#define eo_defer_run _eo_defer_stack.run()
