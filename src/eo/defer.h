1a9c80e02b9ee2aa27bc1d3b33000ea690bdc487
Undeleted user <98804528+4lmighty@users.noreply.github.com>
2026-09-16 10:12:02 +0900

    feat: add function-scoped defer stack

--- src/eo/defer.h
+++ src/eo/defer.h
@@ -3,29 +3,63 @@
 //
 #pragma once
 
+#include <functional>
+#include <memory>
+#include <tuple>
+#include <type_traits>
 #include <utility>
+#include <vector>
 
 namespace eo::detail {
-template<typename F>
-class scope_exit {
+class defer_stack {
+  struct call_base {
+    virtual ~call_base() = default;
+    virtual void invoke() = 0;
+  };
+
+  template<typename F>
+  struct call final : call_base {
+    explicit call(F function): function(std::move(function)) {}
+
+    void invoke() override {
+      std::invoke(function);
+    }
+
+    F function;
+  };
+
 public:
-  explicit scope_exit(F function): function_(std::move(function)) {}
+  defer_stack() = default;
+  defer_stack(const defer_stack&) = delete;
+  defer_stack(defer_stack&&) = delete;
+  auto operator=(const defer_stack&) -> defer_stack& = delete;
+  auto operator=(defer_stack&&) -> defer_stack& = delete;
 
-  scope_exit(const scope_exit&) = delete;
-  scope_exit& operator=(const scope_exit&) = delete;
+  ~defer_stack() noexcept(false) {
+    while (!calls.empty()) {
+      auto deferred = std::move(calls.back());
+      calls.pop_back();
+      deferred->invoke();
+    }
+  }
 
-  ~scope_exit() noexcept {
-    function_();
+  template<typename F, typename... Args>
+  void push(F&& function, Args&&... args) {
+    auto deferred = [function = std::decay_t<F>(std::forward<F>(function)),
+                     args = std::tuple<std::decay_t<Args>...>(std::forward<Args>(args)...)]() mutable {
+      std::apply(
+        [&function](auto&&... stored) {
+          std::invoke(std::move(function), std::forward<decltype(stored)>(stored)...);
+        },
+        std::move(args));
+    };
+    calls.emplace_back(std::make_unique<call<decltype(deferred)>>(std::move(deferred)));
   }
 
 private:
-  F function_;
+  std::vector<std::unique_ptr<call_base>> calls;
 };
-
-template<typename F>
-scope_exit(F) -> scope_exit<F>;
 } // namespace eo::detail
 
-#define EO_CONCAT_IMPL(a, b) a##b
-#define EO_CONCAT(a, b) EO_CONCAT_IMPL(a, b)
-#define eo_defer(...) auto EO_CONCAT(_eo_defer_, __COUNTER__) = eo::detail::scope_exit(__VA_ARGS__)
+#define eo_defer_scope ::eo::detail::defer_stack _eo_defer_stack
+#define eo_defer(...) _eo_defer_stack.push(__VA_ARGS__)
