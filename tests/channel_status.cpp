29806415ac080d7cf42b5e2abe2c1b8b1fad6c63
Undeleted user <98804528+4lmighty@users.noreply.github.com>
2026-09-16 10:32:50 +0900

    style: simplify nil channel suspension test

--- tests/channel_status.cpp
+++ tests/channel_status.cpp
@@ -50,18 +50,14 @@ void test_awaited_nil_operations_stay_suspended() {
   eo::chan<int> nil;
   bool progressed = false;
 
-  auto sender = asio::co_spawn(
-    io,
-    [&]() -> eo::func<> {
-      co_await (nil << 1);
-    },
-    asio::use_future);
-  auto receiver = asio::co_spawn(
-    io,
-    [&]() -> eo::func<> {
-      (void)co_await *nil;
-    },
-    asio::use_future);
+  auto send = [&]() -> eo::func<> {
+    co_await (nil << 1);
+  };
+  auto receive = [&]() -> eo::func<> {
+    (void)co_await *nil;
+  };
+  auto sender = asio::co_spawn(io, send(), asio::use_future);
+  auto receiver = asio::co_spawn(io, receive(), asio::use_future);
 
   asio::post(io, [&] { progressed = true; });
   io.poll();
